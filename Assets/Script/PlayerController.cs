077248faccc34b0df770d360b4573b7a6482d83b
jinseo0517 <jinseo0571@gmail.com>
2025-04-09 16:37:07 +0900

    적이움직이고나따라오고죽는블록있고아이템먹으면커짐무적됨

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -11,25 +11,48 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator pAni;
     private bool isGrounded;
 
-    // Start is called before the first frame update
-    void Start()
+    private bool isGiant = false;
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        pAni = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector3(moveInput * moveSpeed, rb.velocity.y);
+
+        if (isGiant)
+        {
+            if (moveInput < 0)
+                transform.localScale = new Vector3(2f, 2f, 1f);
+
+            if (moveInput > 0)
+                transform.localScale = new Vector3(-2f, 2f, 1f);
+        }
+        else
+        {
+            if (moveInput < 0)
+                transform.localScale = new Vector3(1f, 1f, 1f);
+
+            if (moveInput > 0)
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
         if(isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("JumpAction");
         }
     }
 
@@ -44,5 +67,20 @@ public class PlayerController : MonoBehaviour
             {
             collision.GetComponent<LevelObject>().MoveToNextLevel();
         }
+
+        if (collision.CompareTag("Enemy"))
+        {
+            if (isGiant)
+                Destroy(collision.gameObject);
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (collision.CompareTag("Item"))
+        {
+            isGiant = true;
+            Destroy(collision.gameObject);
+        }
+
     }
 }
